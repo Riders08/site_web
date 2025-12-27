d4dab1332cf2e33e09f394294b0e40fc3bc8a018
Lucas Rivoal <lucas.rivoal@etu.u-bordeaux.fr>
2025-12-27 16:51:06 +0100

    avancée issues code verification, maintenant le mail est envoyé avec un code mais il y a encore des soucis coté vérification de ce dernier

---
+++ Site/src/main/java/com/monsite/Services/VerificationService.java
@@ -0,0 +1,59 @@
+package com.monsite.Services;
+
+import com.monsite.Database.VerificationCodeRepository;
+import com.monsite.models.VerificationCode;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@Service
+public class VerificationService {
+
+    private final VerificationCodeRepository repository;
+    private final MailService mailService;
+
+    public VerificationService(VerificationCodeRepository repository, MailService mailService) {
+        this.repository = repository;
+        this.mailService = mailService;
+    }
+
+    private String generateCode() {
+        return String.valueOf(100000 + new Random().nextInt(900000));
+    }
+
+    public void sendCodeByEmail(String email) throws Exception {
+        String code = generateCode();
+
+        VerificationCode vc = new VerificationCode();
+        vc.setEmail(email);
+        vc.setCode(code);
+        vc.setExpiration(LocalDateTime.now().plusMinutes(5));
+        vc.setVerification(false);
+
+        repository.save(vc);
+        mailService.SendCodeMail(email, code);
+    }
+
+    public boolean verifyCode(String mailPhone, String code) throws Exception {
+        VerificationCode vc = repository.findLastCodeByMail(mailPhone);// Probleme ici c'est null
+        System.out.println(vc);
+        System.out.println(mailPhone);
+        System.out.println(code);
+        if(vc == null){
+            return false;
+        }
+        if(vc.getVerification()){
+            return false;
+        } 
+        if(vc.getExpiration().isBefore(LocalDateTime.now())){
+            return false;
+        }
+
+        if (!vc.getCode().equals(code)){
+            return false;
+        } 
+        repository.markVerified(vc.getID());
+        return true;
+    }
+}
