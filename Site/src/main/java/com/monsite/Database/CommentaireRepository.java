e2f42683753df756cbba8a0831fe23ac7335076b
Lucas Rivoal <lucas.rivoal@etu.u-bordeaux.fr>
2025-12-28 17:58:47 +0100

    initialisation d'une database des commentaires

---
+++ Site/src/main/java/com/monsite/Database/CommentaireRepository.java
@@ -0,0 +1,58 @@
+package com.monsite.Database;
+
+import org.springframework.stereotype.Repository;
+import com.monsite.models.Commentaire;
+import java.util.ArrayList;
+import java.sql.*;
+
+
+public class CommentaireRepository {
+    private final Database database;
+
+    public CommentaireRepository(Database database){
+        this.database = database;
+    }
+
+    public ArrayList<Commentaire> getCommentaires() throws SQLException{
+        String sql = "SELECT id, users, commentaire FROM commentaire";
+        ArrayList<Commentaire> ListCommentaire = new ArrayList<>();
+        try(Connection conn= this.database.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)
+            ) {
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Commentaire newCommentaire = new Commentaire();
+                newCommentaire.setID(rs.getLong("id"));
+                newCommentaire.setUser(rs.getString("user"));
+                newCommentaire.setCommentaire(rs.getString("commentaire")); 
+                ListCommentaire.add(newCommentaire);
+            }
+        }
+        return ListCommentaire;
+    }
+
+    public void clearTable() throws SQLException{
+        String sql = "TRUNCATE TABLE commentaire RESTART IDENTITY";
+        try(Connection conn = this.database.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+                pstmt.executeUpdate();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void AddCommentaire(Commentaire commentaire)throws SQLException{
+        String sql = "INSERT INTO commentaire (user, commentaire) VALUES (?, ?)";
+        try(Connection conn = this.database.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+                pstmt.setString(1, commentaire.getUser());
+                pstmt.setString(2, commentaire.getCommentaire());
+                int rows = pstmt.executeUpdate();
+                if(rows > 0){
+                    System.out.println("✅ L'insertion du commentaire de l'utilisateur "+ commentaire.getUser() + " a été effectué avec succès !");
+                }else{
+                    System.out.println("⚠️  L'insertion du commentaire de l'utilisateur "+ commentaire.getUser() + " a échoué !");
+                }
+            }
+    }
+}
