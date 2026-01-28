deb2eb92c3f1df7dae8c25b828bba0ba5ff671f9
Lucas Rivoal <lucas.rivoal@etu.u-bordeaux.fr>
2026-01-28 17:59:34 +0100

    ajout d'une partie frontend interessant style theme ect.. + demarrage redirection swipper via barre de taches

--- Site/src/main/resources/static/etudes.js
+++ Site/src/main/resources/static/etudes.js
@@ -1,14 +1,60 @@
- const swiper = new Swiper('.swiper',{
+import { etudes } from "./typescript.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    Object.keys(etudes).forEach(element =>{
+        document.querySelector(`.divers_sco ${element}`).addEventListener("click", (e) =>{
+            if(e.ctrlKey){
+                window.open(etudes[element]);
+            }else{
+                window.location.href = etudes[element];
+            }
+        })
+    });
+})
+
+const swiper = new Swiper('.swiper',{
     direction: 'horizontal',
     loop: false,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
-    }
- });
+    },
+    on : {
+        init: function () {
+            const hash = window.location.hash;
 
-gsap.from(".niveau", {
-    y:-100,
-    opacity:0,
-    duration:1,
- });
+            switch(hash){
+                case "#primaire" :
+                    swiper.slideTo(1);
+                    break;
+                case "#college":
+                    swiper.slideTo(2);
+                    break;
+                case "#lycee":
+                    swiper.slideTo(3);
+                    break;
+                case "#universite":
+                    swiper.slideTo(4);
+                    break;
+                default:
+                    swiper.slideTo(0);
+            }
+            gsap.from(".niveau", {
+                y:-100,
+                opacity:0,
+                duration:1,
+            })
+        },
+        slideChange: function () {
+            gsap.from(".niveau", {
+                y:-100,
+                opacity:0,
+                duration:1,
+            });
+        }
+    }
+});
+
+
+
