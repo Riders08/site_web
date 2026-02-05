5056c192cc981857e3e7a507eb84ed6fb392be06
Lucas Rivoal <lucas.rivoal@etu.u-bordeaux.fr>
2026-02-05 15:04:55 +0100

    modification de la barre plus partie swipper presentation effectuer et fini

--- Site/target/classes/static/etudes.js
+++ Site/target/classes/static/etudes.js
@@ -1,18 +1,3 @@
-import { etudes } from "./typescript.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    Object.keys(etudes).forEach(element =>{
-        document.querySelector(`.divers_sco ${element}`).addEventListener("click", (e) =>{
-            if(e.ctrlKey){
-                window.open(etudes[element]);
-            }else{
-                window.location.href = etudes[element];
-            }
-        })
-    });
-})
-
 const swiper = new Swiper('.swiper',{
     direction: 'horizontal',
     loop: false,
@@ -20,41 +5,71 @@ const swiper = new Swiper('.swiper',{
         el: ".swiper-pagination",
         clickable: true,
     },
+    mousewheel: {
+        forceToAxis: true,
+        sensitivity: 0.05,
+        thresholdDelta: 150,
+        thresholdTime: 1000,
+        releaseOnEdges: false
+    },
+    keyboard: {
+        enabled: true,
+    },
     on : {
         init: function () {
-            const hash = window.location.hash;
-
-            switch(hash){
-                case "#primaire" :
-                    swiper.slideTo(1);
-                    break;
-                case "#college":
-                    swiper.slideTo(2);
-                    break;
-                case "#lycee":
-                    swiper.slideTo(3);
-                    break;
-                case "#universite":
-                    swiper.slideTo(4);
-                    break;
-                default:
-                    swiper.slideTo(0);
-            }
-            gsap.from(".niveau", {
+            const timeline_top = gsap.timeline();
+            const timeline_bottom = gsap.timeline();
+            timeline_top.from(".niveau", {
                 y:-100,
                 opacity:0,
                 duration:1,
-            })
+            }).from(".subtitle", {
+                y:-100,
+                opacity:0,
+                duration:1,
+
+            }, "-=0.5");
+            timeline_bottom.from(".direction", {
+                y:100,
+                opacity:0,
+                duration:1,
+            }).from(".paragraphe", {
+                y:100,
+                opacity:0,
+                duration:1,
+
+            }, "-=0.5");
         },
         slideChange: function () {
-            gsap.from(".niveau", {
+            const timeline_top = gsap.timeline();
+            const timeline_bottom = gsap.timeline();
+            timeline_top.from(".niveau", {
                 y:-100,
                 opacity:0,
                 duration:1,
-            });
+            }).from(".subtitle", {
+                y:-100,
+                opacity:0,
+                duration:1,
+            }, "-=0.5");
+            timeline_bottom.from(".direction", {
+                y:100,
+                opacity:0,
+                duration:1,
+            }).from(".paragraphe", {
+                y:100,
+                opacity:0,
+                duration:1,
+            }, "-=0.5");
         }
     }
 });
 
+document.querySelectorAll(".fa-arrow-right").addEventListener("click", (e) => {
+    swiper.slideNext();
+});
 
+document.querySelectorAll(".fa-arrow-left").addEventListener("click", (e) => {
+    swiper.slidePrevious();
+});
 
